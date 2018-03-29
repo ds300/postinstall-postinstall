f74d9a9b54aedb1a3c4ab4ad700c9d928dd80687
David Sheldrick <david@futurice.com>
2018-03-29 01:01:44 +0100

    initial commit

---
+++ run.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+const cwd = require('process').cwd()
+const path = require('path')
+const exec = require('child_process').execSync
+
+const appPath = path.normalize(cwd.slice(0, cwd.lastIndexOf('node_modules')))
+
+const packageJsonPath = path.join(appPath, 'package.json')
+
+if (fs.existsSync(packageJsonPath)) {
+  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath))
+
+  if (packageJson.scripts && packageJson.scripts.postinstall) {
+    exec('npm run postinstall', {cwd: appPath})
+  }
+}
+
+
